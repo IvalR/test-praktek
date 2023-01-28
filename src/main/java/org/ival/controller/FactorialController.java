4e28e5ab366aca40d919aa7da7cd8cacda994970
Ival Racha Pratama <ivalivalrakha@gmail.com>
2023-01-28 14:33:27 +0700

    factorial

--- src/main/java/org/ival/controller/FactorialController.java
+++ src/main/java/org/ival/controller/FactorialController.java
@@ -1,2 +1,54 @@
-package org.ival.controller;public class FactorialController {
+package org.ival.controller;
+
+import org.ival.model.Employee;
+import org.ival.model.FactorialModel;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("factorial")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class FactorialController {
+
+    public Connection connection;
+
+    public FactorialController(){
+        try {
+            connection = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/green-cafe","postgres","adwed");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GET
+    @Path("{input}")
+    public Response get(@PathParam("input")Long input){
+        List<FactorialModel> factorial = new ArrayList<>();
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("WITH RECURSIVE cte (n, factorial) AS (" +
+                    " VALUES (0, 1)" +
+                    " UNION ALL" +
+                    " SELECT n+1, (n+1)*factorial FROM cte WHERE n < "+ input+")" +
+                    " SELECT * FROM cte");
+            try (ResultSet resultSet = statement.getResultSet()) {
+                while (resultSet.next()) {
+                    FactorialModel factorialModel = new FactorialModel();
+                    factorialModel.setN(resultSet.getInt("n"));
+                    factorialModel.setFactorial(resultSet.getInt("factorial"));
+                    factorial.add(factorialModel);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Response.ok(factorial).build();
+    }
 }
--- src/main/java/org/ival/model/FactorialModel.java
+++ src/main/java/org/ival/model/FactorialModel.java
@@ -3,15 +3,41 @@ package org.ival.model;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "factorial")
 public class FactorialModel {
+    public Integer getN() {
+        return n;
+    }
+
+    public void setN(Integer n) {
+        this.n = n;
+    }
+
+    public Integer getFactorial() {
+        return factorial;
+    }
+
+    public void setFactorial(Integer factorial) {
+        this.factorial = factorial;
+    }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "n")
     public Integer n;
 
+    @Column(name = "factorial")
     public Integer factorial;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
